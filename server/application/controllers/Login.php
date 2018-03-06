4a9feba941455ebb86924768b96c4c91b9e8e401
wangj28 <wangj28@mingyuanyun.com>
2018-03-06 18:13:27 +0800

    完善登录接口

--- server/application/controllers/Login.php
+++ server/application/controllers/Login.php
@@ -18,8 +18,32 @@ class Login extends CI_Controller {
             "Pragma" => "no-cache",
             "upgrade-insecure-request" => 1
         );
-        $result = $this->curl_https($url, $form_data, $headers, "PB3_SESSION=".$pb3_session, 30, true);
-        var_dump($result);
+        $result = $this->curl_https($url, $form_data, $headers, "PB3_SESSION=".$pb3_session, 30, false);
+        preg_match('/A2\=\"(.*)\"\;/', $result, $matches);
+        var_dump($result, $matches);
+
+        if (!empty($matches[1])) {
+            $A2 = $matches[1];
+            $this->input->set_cookie(array(
+                'name' => 'PB3_SESSION',
+                'value' => $pb3_session
+            ));
+            $this->input->set_cookie(array(
+                'name' => 'A2',
+                'value' => $A2
+            ));
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode(array('code' => '200', 'msg' => '登录成功')))
+                ->_display();
+
+        } else {
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode(array('code' => '10001', 'msg' => '登录失败')))
+                ->_display();
+            exit;
+        }
 //        var_dump($pb3_session);
 
     }
@@ -32,18 +56,22 @@ class Login extends CI_Controller {
      * @param int    $timeout    超时时间，默认30s
      * @param bool   $debug      是否打印错误信息，默认false
      */
-    public function curl_https($url, $data=array(), $header=array(), $cookie, $timeout=30, $debug=false){
-
+    private function curl_https($url, $data=array(), $header=array(), $cookie, $timeout=60, $debug=false){
+        //$proxy = $this->get_proxy();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // 从证书中检查SSL加密算法是否存在
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch,CURLOPT_COOKIE,$cookie);
+        //curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        curl_setopt($ch, CURLOPT_REFERER, $header['Referer']);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt ( $ch, CURLOPT_CONNECTTIMEOUT, 30 );
+        curl_setopt ( $ch, CURLOPT_HEADER, true );
 
         $response = curl_exec($ch);
         preg_match('/Set-Cookie:(.*);/iU',$response,$str);
@@ -65,7 +93,6 @@ class Login extends CI_Controller {
             echo '=====$response====='."\r\n";
             print_r( $response );
             print_r($str);
-
         }
         curl_close($ch);
 
@@ -73,8 +100,15 @@ class Login extends CI_Controller {
 
     }
 
+    private function get_proxy() {
+        //$proxy = json_decode(file_get_contents('https://api.getproxylist.com/proxy?allowsHttps=1&protocol[]=http&country=CN'));
+        //return $proxy->ip.":".$proxy->port;
+        return "139.198.12.227:3128";
+    }
+
 
     public function create() {
         $this->load->view('welcome_message');
     }
 }
+
